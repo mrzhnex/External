2dc686fd92c03f92eaf7c332fc2c8671c76dc551
Tabula <MrZhnex@ya.ru>
2020-07-03 01:02:50 +0300

    Удален класс Kudash. Статичный метод класса Kudash принимал двумерный массив int[,] и проверял на магический круг. В классе Program в вечном цикле создавался массив и заполнялся случайными значаниями (от 0 до 3, вроде) и проверялся с помощью статичного метода в классе Kudash.

--- Program.cs
+++ Program.cs
@@ -8,39 +8,6 @@ namespace External
         {
             Console.WriteLine("Hello world!");
 
-
-
-            while (true)
-            {
-                int[,] mass = new int[4, 4];
-                Random random = new Random();
-                for (int i = 0; i < mass.GetLength(0); i++)
-                {
-                    for (int k = 0; k < mass.GetLength(1); k++)
-                    {
-                        mass[i, k] = random.Next(0, 4);
-                        //Console.Write(mass[i, k] + " ");
-                    }
-                    //Console.WriteLine();
-                }
-
-                if (Kudash.IsMagicSquare(mass))
-                {
-                    Console.WriteLine("Ура. Кудаш сдал ЕГЭ");
-                    for (int i = 0; i < mass.GetLength(0); i++)
-                    {
-                        for (int k = 0; k < mass.GetLength(1); k++)
-                        {
-                            mass[i, k] = random.Next(0, 4);
-                            Console.Write(mass[i, k] + " ");
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.ReadKey();
-                }
-            }
-
-
             ExInt exInt = 0;
             while (true)
             {
